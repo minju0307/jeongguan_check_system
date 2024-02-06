685252c7c8752d8129749274ff7bc8a544fbaaee
J.P Lee <koreanfeel@gmail.com>
2024-02-06 21:32:50 +0900

    문단 검색 시 GPU를 사용하도록 코드 수정

--- inference_paragraph.py
+++ inference_paragraph.py
@@ -41,17 +41,21 @@ def semantic_search(question, input_texts, top_k):
 
 class SemanticSearch:
     def __init__(self):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
         model_path = "multilabel_model"
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
         self.model = AutoModelForSequenceClassification.from_pretrained(
             model_path, output_hidden_states=True
-        )
+        ).to(self.device)
 
     def get_embedding(self, sentences):
         encoded_input = self.tokenizer(
             sentences, padding=True, truncation=True, return_tensors="pt"
         )
+        encoded_input.to(self.device)
+
         with torch.no_grad():
             model_output = self.model(**encoded_input)
         last_hidden_states = model_output.hidden_states[-1]
@@ -63,6 +67,9 @@ class SemanticSearch:
 
         target_embedding = self.get_embedding([question])
         sentence_embeddings = self.get_embedding(input_texts)
+
+        target_embedding = target_embedding.cpu().numpy()
+        sentence_embeddings = sentence_embeddings.cpu().numpy()
 
         similarities = cosine_similarity(target_embedding, sentence_embeddings)
         top_k_indices = np.argsort(similarities[0])[-top_k:][::-1]
