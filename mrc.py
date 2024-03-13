611dd7268f076b27c012c21e854a5311bec0b89e
J.P Lee <koreanfeel@gmail.com>
2024-03-13 17:33:39 +0900

    프롬프트 템플릿을 별도 파일로 분리

--- mrc.py
+++ mrc.py
@@ -3,6 +3,7 @@ import traceback
 import openai
 
 from config import DEBUG
+from prompt.template import ANSWER_TEMPLATE
 from utils.utils import print_exception
 
 
@@ -16,11 +17,7 @@ def generate_gpt(gpt_ver, query):
 
 
 def generate_answer(gpt_ver, jeongguan, question):
-    template = """정관 문서: {paragraph}
-    질문: {question}
-    ===
-    너는 변호사로서, 회사 정관을 검토해야 한다. 선생님께 정관 문서를 힌트로 받았다. 다음 질문에 대한 답은? 진위형 질문이면 예, 또는 아니오로 대답하고, 주관식 질문이면 한 줄로 대답하시오.
-    """
+    template = ANSWER_TEMPLATE
 
     try:
         query = template.format(paragraph=jeongguan, question=question)
--- tests/llm_test.py
+++ tests/llm_test.py
@@ -8,6 +8,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from config import GPT_MODEL, OPENAI_API_KEY, LANGSMITH_API_KEY
 from main import get_advice
 from mrc import generate_answer
+from prompt.template import ADVICE_TEMPLATE, ANSWER_TEMPLATE
 
 
 class TestLLM(unittest.TestCase):
@@ -47,11 +48,7 @@ class TestLLM(unittest.TestCase):
 
         llm = ChatOpenAI(model=self.gpt_model)
 
-        prompt = ChatPromptTemplate.from_template("""정관 문서: {paragraph}
-질문: {question}
-===
-너는 변호사로서, 회사 정관을 검토해야 한다. 선생님께 정관 문서를 힌트로 받았다. 다음 질문에 대한 답은? 진위형 질문이면 예, 또는 아니오로 대답하고, 주관식 질문이면 한 줄로 대답하시오.
-        """)
+        prompt = ChatPromptTemplate.from_template(ANSWER_TEMPLATE)
 
         chain = prompt | llm
 
@@ -101,42 +98,7 @@ class TestLLM(unittest.TestCase):
 """
         llm = ChatOpenAI(model=self.gpt_model)
 
-        prompt = ChatPromptTemplate.from_template("""[회사의 정관 검토 내역]
-Q.감사의 임기는 얼마인가? A.2년
-
-[법령]
-상법 제410조
- 감사의 임기는 취임 후 3년내의 최종의 결산기에 관한 정기총회의 종결시까지로 한다.
-
-상법 제312조
- 감사는 한 명이상이어야 하지만, 자본금 총액 10억원 미만의 회사의 경우에는 감사를 선임하지 않을 수있다. 
-
-상법 제383조 제2항
- 이사의 임기는 3년을 초과하지 못한다.
-
-[hint]
- 가장 관련있는 1가지의 [법령]을 reference해서 변호사로서 정관 체크리스트의 결과가 법적으로 올바른지 적절한 조언을 생성해줘. 직접적으로 관련이 없는 법령은 참고하지마. you should generate in 256 tokens. 한국어로 답변 해줘.
-
-[question]
-회사의 정관을 검토해본 결과 'Q.감사의 임기는 얼마인가? A.2년'라는 것을 알 수 있었어. 이 정관이 법적으로 올바르게 쓰여진거야? 이에 대한 답변을 해야 해. [hint]를 활용하여 답변해봐.
-
-[조언]
-귀사의 정관은 감사의 임기를 2년이라고 규정하고 있습니다. 상법 제410조는 감사의 임기를 취임 후 3년내의 최종의 결산기에 관한 정기총회의 종결시까지로 정하고 있습니다.
-
-[회사의 정관 검토 내역]
-Q.{question} A.{answer}
-
-[법령]
-{retrieval_results}
-
-[hint]
- 가장 관련있는 1가지의 [법령]을 reference해서 변호사로서 정관 체크리스트의 결과가 법적으로 올바른지 적절한 조언을 생성해줘. 직접적으로 관련이 없는 법령은 참고하지마. you should generate in 256 tokens. 한국어로 답변 해줘.
-
-[question]
-회사의 정관을 검토해본 결과 'Q.{question} A.{answer}'라는 것을 알 수 있었어. 이 정관이 법적으로 올바르게 쓰여진거야? 이에 대한 답변을 해야 해. [hint]를 활용하여 답변해봐.
-
-[조언]
-""")
+        prompt = ChatPromptTemplate.from_template(ADVICE_TEMPLATE)
         chain = prompt | llm
 
         result = chain.invoke({
